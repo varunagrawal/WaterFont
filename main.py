d3dcd667d0614f498dbab53622978812d28a5e8d
varunagrawal <varagrawal@gmail.com>
2013-05-15 03:11:24 +0530

    Added font module

--- main.py
+++ main.py
@@ -1,44 +1,11 @@
 #!/usr/bin/python
 
-import PIL
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
+import font
 
-def select_font( font_path ):
-	font = ImageFont.truetype( font_path, 20 )
+# Create GUI. Use TKinter for cross-platform support
 
-	return font
-
-def main():
-	font_file = "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf"
-	font = select_font(font_file)
-
-	img = Image.open("orig.jpg")
-	
-	text = "Varun Agrawal"
-
-	#Transparency of Text
-	trans = 120
-
-	#The Text to be written will be white with trans as the alpha value
-	t_color = (0, 0, 0, trans)
-	text_pos = (img.size[0]-170, img.size[1]-25)
-
-	
-	#Specify alpha as 0 to get transparent image on which to write
-	watermark = Image.new("RGBA", img.size, (255, 255, 255, 0))
-
-
-	waterdraw = ImageDraw.Draw(watermark, "RGBA")
-	waterdraw.text(text_pos, text, fill=t_color, font=font)
-
-	
-	#watermark.save("watermark.jpg")
-	
-	img.paste(watermark, None, watermark)
-	img.save("temp-wm.jpg")	
-
-
-if __name__ == "__main__":
-	main()
+#Should display the image
+#Should be able to load fonts
+#Should have a slider for opacity
+#Should show the watermark
+#Option for multiple images
